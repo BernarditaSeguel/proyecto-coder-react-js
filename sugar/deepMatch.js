09421ba0bee4b91ab873c5f81d414eddcbfb608d
BernarditaSeguel <ber.seguel@duocuc.cl>
2022-07-10 20:02:38 -0400

    4

--- sugar/deepMatch.js
+++ sugar/deepMatch.js
@@ -2,3 +2,7 @@ let { a: val } = { a: 2};
 let { propName: nombre } = { propName: 'valor' };
 
 console.log(nombre);
+
+let { nombre:nombrePersona, edad: edadPersona, } = {nombre: "josefina", edad:"3"};
+console.log(nombrePersona);
+console.log(edadPersona);
---
+++ sugar/destructuracion.js
@@ -0,0 +1,19 @@
+const objeto = {
+    nombre: 'Josefina',
+    apellido: 'Carrasco',
+    edad: 3,
+    país: 'Chile',
+};
+
+// let { nombre, apellido } = objeto;
+
+let { nombre, apellido } = props;
+console. log(nombre);
+console. log(apellido);
+
+// let { edad } = objeto;
+let { edad } = props;
+console. log(edad);
+
+// console. log(objeto.nombre);
+// console. log(objeto.apellido);
